a479af5873e2666bb457dfa6a60fe80b792c841b
Peter Vang <vang_peter06@yahoo.com>
2022-06-17 14:14:18 -0700

    Change sizePizza to addTotalCost, complete test 2.

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ Test: It should return a Pizza object with two properties for topping and size
 code: let newOrder = new Pizza (["pepperoni", "pineapple"], "medium")
 Expected Output : newOrder {toppings: {["pepperoni", "pineapple"], size: "medium", price: 0}
 
-Describe: sizePizza
+Describe: addTotalCost
 
 Test: "It should price the pizza depending on the size is order."
-Code: let newOrder = new Pizza (["pepperoni", "pineapple"], "medium")
-Expect Output: newOrder {toppings: {["pepperoni", "pineapple"], size: "medium", price: 10}
+Code: 
+let newOrder = new Pizza (["pepperoni", "pineapple"], "medium")
+newOrder.addTotalCost();
+Expect Output: 10
+
+Test: "It should price the pizza depending on the size and toppings is order."
+Code: 
+let newOrder = new Pizza (["pepperoni", "pineapple"], "medium")
+newOrder.addTotalCost();
+Expect Output:12
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 
 
-function Pizza(meat, veggies, size) {
-  this.meat = meat;
-  this.veggies = veggies;
+function Pizza(toppings, size) {
+  this.toppings =toppings
   this.size = size;
   this.price = 0;
 } 
 
-Pizza.prototype.sizePizza = function() {
-  // let price = 0;
+Pizza.prototype.addTotalCost = function() {
   if (this.size === "small") {
-    this.price = 5;
+    this.price += 5;
   } else if (this.size === "medium") {
-    this.price = 10;
+    this.price += 10;
   } else {
-    this.price = 20;
-  } 
+    this.price += 20;
+  } ;
+  if(this.toppings.includes("pepperoni")){
+    this.price += 1;
+  };
+  if(this.toppings.includes("extra cheese")){
+    this.price += 1;
+  };
+  if(this.toppings.includes("anchovies")){
+    this.price += 1;
+  };
+  if(this.toppings.includes("pineapple")){
+    this.price += 1;
+  };    
   return this.price;
-}
+};
 
 
-let newOrder = new Pizza ("pepperoni","pineapple", "medium", price)
+let newOrder = new Pizza (["pepperoni","pineapple"], "medium")
